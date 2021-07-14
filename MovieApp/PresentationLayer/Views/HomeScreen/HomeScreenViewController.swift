f192e3c5f71fe7a0fb00e100d7f72045ef54047f
Luka <53893982+lukan98@users.noreply.github.com>
2021-07-14 12:13:25 +0200

    Collection view added. Layout set up.

--- MovieApp/PresentationLayer/Views/HomeScreen/HomeScreenViewController.swift
+++ MovieApp/PresentationLayer/Views/HomeScreen/HomeScreenViewController.swift
@@ -3,12 +3,64 @@ import UIKit
 class HomeScreenViewController: UIViewController {
 
     var navigationView: NavBarView!
+    var movieCollection: UICollectionView!
 
     private var homeScreenPresenter: HomeScreenPresenter!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         buildViews()
+        setUpCollectionView()
+    }
+
+    private func setUpCollectionView() {
+        movieCollection.dataSource = self
+        movieCollection.delegate = self
+        movieCollection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        movieCollection.collectionViewLayout.invalidateLayout()
+    }
+
+}
+
+// MARK: CollectionView Data Source
+
+extension HomeScreenViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = movieCollection.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+
+}
+
+// MARK: CollectionViewDelegate Flow Layout
+
+extension HomeScreenViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(width: collectionView.bounds.width - 36, height: 140)
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+        UIEdgeInsets(top: 22, left: 0, bottom: 6, right: 0)
     }
 
 }
