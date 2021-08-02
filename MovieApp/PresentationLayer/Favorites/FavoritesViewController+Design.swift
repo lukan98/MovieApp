2eb1f8e52c2891d4056d14bc4ee7055f509793dd
Luka <53893982+lukan98@users.noreply.github.com>
2021-08-02 10:52:35 +0200

    Favorites UI implemented

--- MovieApp/PresentationLayer/Favorites/FavoritesViewController+Design.swift
+++ MovieApp/PresentationLayer/Favorites/FavoritesViewController+Design.swift
@@ -15,10 +15,24 @@ extension FavoritesViewController: ConstructViewsProtocol {
     func createViews() {
         navigationView = NavBarView()
         view.addSubview(navigationView)
+
+        favoritesLabel = UILabel()
+        view.addSubview(favoritesLabel)
+
+        movieCollectionView = makeCollectionView()
+        view.addSubview(movieCollectionView)
     }
 
     func styleViews() {
         view.backgroundColor = .white
+
+        favoritesLabel.font = UIFont(name: "ProximaNova-Bold", size: 20)
+        favoritesLabel.textColor = UIColor(named: "DarkBlue")
+        favoritesLabel.text = "Favorites"
+
+        movieCollectionView.backgroundColor = .white
+        movieCollectionView.showsVerticalScrollIndicator = false
+
     }
 
     func defineLayoutForViews() {
@@ -26,6 +40,28 @@ extension FavoritesViewController: ConstructViewsProtocol {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(NavBarView.defaultHeight)
         }
+
+        favoritesLabel.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom).offset(35)
+            $0.leading.equalToSuperview().offset(20)
+        }
+
+        movieCollectionView.snp.makeConstraints {
+            $0.top.equalTo(favoritesLabel.snp.bottom).offset(20)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+
+    private func makeCollectionView() -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(MoviePosterCell.self, forCellWithReuseIdentifier: MoviePosterCell.cellIdentifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        return collectionView
     }
 
 }
--- MovieApp/PresentationLayer/Favorites/FavoritesViewController.swift
+++ MovieApp/PresentationLayer/Favorites/FavoritesViewController.swift
@@ -3,11 +3,81 @@ import UIKit
 class FavoritesViewController: UIViewController {
 
     var navigationView: NavBarView!
+    var favoritesLabel: UILabel!
+    var movieCollectionView: UICollectionView!
+
+    private let inset: CGFloat = 20
+    private let verticalSpacing: CGFloat = 35
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         buildViews()
+    }
+
+}
+
+extension FavoritesViewController: UICollectionViewDataSource {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        100
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard
+            let cell = movieCollectionView.dequeueReusableCell(
+                withReuseIdentifier: MoviePosterCell.cellIdentifier,
+                for: indexPath) as? MoviePosterCell
+        else {
+            let cell = MoviePosterCell()
+            return cell
+        }
+
+        let movie = MovieViewModel(
+            id: 1,
+            about: "",
+            name: "",
+            posterSource: "https://image.tmdb.org/t/p/w185/rYFAvSPlQUCebayLcxyK79yvtvV.jpg",
+            genres: [],
+            isFavorited: true)
+        
+        cell.setData(for: movie)
+        cell.moviePoster.onFavoriteToggle = { _ in }
+        return cell
+    }
+
+}
+
+extension FavoritesViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        CGSize(width: 105, height: 155)
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+        UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        verticalSpacing
     }
 
 }
