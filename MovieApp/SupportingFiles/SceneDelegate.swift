8832388dcd2e66abf35dfe12e2424aae9b9be62a
Luka <53893982+lukan98@users.noreply.github.com>
2021-08-16 12:53:26 +0200

    Starting HomeScreenViewController replaced with TabBarController

--- MovieApp/SupportingFiles/SceneDelegate.swift
+++ MovieApp/SupportingFiles/SceneDelegate.swift
@@ -20,35 +20,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         router?.start(in: window)
     }
 
-    private func makeUITabBarController() -> UITabBarController {
-        let tabBarController = UITabBarController()
-        tabBarController.tabBar.tintColor = UIColor(named: "DarkBlue")
-        tabBarController.tabBar.isTranslucent = false
-
-        let styledFont = UIFont(name: "ProximaNova-Medium", size: 10)
-
-        let homeScreen = HomeScreenViewController(
-            presenter: HomeScreenPresenter(
-                movieUseCase: appDependencies.movieUseCase,
-                genreUseCase: appDependencies.genreUseCase))
-        homeScreen.styleForTabBar(
-            title: "Home",
-            image: UIImage(named: "Home-outline"),
-            selectedImage: UIImage(named: "Home-fill"),
-            font: styledFont)
-
-        let favorites = FavoritesViewController(presenter: FavoritesPresenter(useCase: appDependencies.movieUseCase))
-        favorites.styleForTabBar(
-            title: "Favorites",
-            image: UIImage(named: "Favorites-outline"),
-            selectedImage: UIImage(named: "Favorites-fill"),
-            font: styledFont)
-
-        tabBarController.viewControllers = [homeScreen, favorites]
-
-        return tabBarController
-    }
-
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
