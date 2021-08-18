713c1fb43e1ab39e446ec4e9414df395bc207788
Luka <53893982+lukan98@users.noreply.github.com>
2021-08-18 09:39:25 +0200

    Update UIColor+Extensions.swift

--- MovieApp/SupportingFiles/Extensions/UIColor+Extensions.swift
+++ MovieApp/SupportingFiles/Extensions/UIColor+Extensions.swift
@@ -3,7 +3,7 @@ import UIKit
 extension UIColor {
 
     static let darkBlue = UIColor(named: "DarkBlue")!
-    static let gray = UIColor(named: "Gray")
+    static let gray = UIColor(named: "Gray")!
     static let gray2 = UIColor(named: "Gray2")!
     static let gray3 = UIColor(named: "Gray3")!
     static let darkGreen = UIColor(named: "DarkGreen")!
