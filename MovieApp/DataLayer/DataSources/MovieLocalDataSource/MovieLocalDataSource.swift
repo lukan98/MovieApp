2580de1c7a9407429beb25dbbf077fd5d02d1326
Luka <53893982+lukan98@users.noreply.github.com>
2021-09-17 15:25:05 +0200

    Save and details functions implemented

--- MovieApp/DataLayer/DataSources/MovieLocalDataSource/MovieLocalDataSource.swift
+++ MovieApp/DataLayer/DataSources/MovieLocalDataSource/MovieLocalDataSource.swift
@@ -33,6 +33,22 @@ class MovieLocalDataSource: MovieLocalDataSourceProtocol {
             .eraseToAnyPublisher()
     }
 
+    func details(for movieId: Int) -> AnyPublisher<DetailedMovieDataSourceModel, Error> {
+        guard let realm = try? Realm()
+        else {
+            print("Couldn't create realm")
+            return .never()
+        }
+
+        return realm
+            .objects(DetailedMovieLocalDataSourceModel.self)
+            .filter { $0.id == movieId }
+            .publisher
+            .map { $0.toDataSourceModel() }
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
     func trendingMovies(for timeWindow: TimeWindowDataSourceModel) -> AnyPublisher<[MovieDataSourceModel], Error> {
         movies
             .map { $0.filter { $0.categories.contains(CategoryDataSourceModel(from: timeWindow)) } }
@@ -61,6 +77,24 @@ class MovieLocalDataSource: MovieLocalDataSourceProtocol {
                             update: .modified)
                     }
                 }
+            }
+        } catch {
+            print("An error ocurred while writing to the realm")
+        }
+    }
+
+    func save(_ movie: DetailedMovieDataSourceModel) {
+        guard let realm = try? Realm()
+        else {
+            print("Couldn't create realm")
+            return
+        }
+
+        do {
+            try realm.write {
+                realm.add(
+                    DetailedMovieLocalDataSourceModel(from: movie),
+                    update: .modified)
             }
         } catch {
             print("An error ocurred while writing to the realm")
--- MovieApp/DataLayer/DataSources/MovieLocalDataSource/MovieLocalDataSourceProtocol.swift
+++ MovieApp/DataLayer/DataSources/MovieLocalDataSource/MovieLocalDataSourceProtocol.swift
@@ -7,6 +7,10 @@ protocol MovieLocalDataSourceProtocol {
 
     func trendingMovies(for timeWindow: TimeWindowDataSourceModel) -> AnyPublisher<[MovieDataSourceModel], Error>
 
+    func details(for movieId: Int) -> AnyPublisher<DetailedMovieDataSourceModel, Error>
+
     func save(_ movies: [MovieDataSourceModel], with category: CategoryDataSourceModel)
+
+    func save(_ movie: DetailedMovieDataSourceModel)
 
 }
